3647782958a4ff04b2b556247995d40bafd8a19d
Steven Espinosa <stevenespinosa@steeleconsult.com>
2019-06-30 23:21:36 -0600

    Initial commit

---
+++ xml.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func errorCheck(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	// Make sure we have at least two args and second isn't empty.  Second arg is URL; first is command executed
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		fmt.Printf("Usage: %s %s [output file]\n", os.Args[0], "https://api.somesite.com/odata/$metadata")
+		os.Exit(1)
+	}
+
+	// Default output file name if none provided
+	outputFile := "entities.ts"
+
+	//Check that we have a URL to request from
+	metadataUrl := os.Args[1]
+
+	//If there are more than two args we assume the third is the output filename.  Path is relative to execution if not provided
+	if len(os.Args) > 2 && os.Args[2] != "" {
+		outputFile = os.Args[2]
+	}
+
+	//Retrieve EDM metadata from odata service
+	metadataResponse, err := http.Get(metadataUrl)
+	errorCheck(err)
+
+	defer metadataResponse.Body.Close()
+
+	// Convert file to byte[] so it can be unmarshal'd
+	byteValue, err := ioutil.ReadAll(metadataResponse.Body)
+	errorCheck(err)
+
+	var edmx Edmx
+	var outputString string
+
+	//Unmarshal retrieved edm data into our edmx classes
+	xmlError := xml.Unmarshal(byteValue, &edmx)
+	errorCheck(xmlError)
+
+	//Loop through dataservices->schemas->entity types to get to the properties for each entity
+	for _, dataService := range edmx.DataServices {
+		for _, schema := range dataService.Schemas {
+			for _, entityType := range schema.EntityTypes {
+
+				outputString += "export class " + entityType.Name + " {\n"
+
+				for _, entityData := range entityType.Properties {
+					var dataType = "string"
+					switch entityData.Type {
+					case "Edm.Int32":
+						dataType = "number"
+					case "Edm.Int64":
+						dataType = "number"
+					case "Edm.Decimal":
+						dataType = "number"
+					case "Edm.Boolean":
+						dataType = "boolean"
+					}
+
+					outputString += "\t" + entityData.Name + ": " + dataType + "\n"
+				}
+
+				outputString += "}" + "\n\n"
+			}
+		}
+	}
+
+	//fmt.Print(outputString)
+
+	//Write the string buffer to the output file.  Perm 0644 is owner read/write and all read
+	fileErr := ioutil.WriteFile(outputFile, []byte(outputString), 0644)
+	errorCheck(fileErr)
+
+}
---
+++ xmlTypes.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/xml"
+)
+
+type Edmx struct {
+	XMLName      xml.Name      `xml:"Edmx"`
+	DataServices []DataService `xml:"DataServices"`
+	edmx         string        `xml:"xmlns,attr"`
+	Version      string        `xml:"Version,attr"`
+}
+
+type DataService struct {
+	XMLName   xml.Name `xml:"DataServices"`
+	Schemas   []Schema `xml:"Schema"`
+	XMLNS     string   `xml:"xmlns,attr"`
+	Namespace string   `xml:"Namespace,attr"`
+}
+
+type Schema struct {
+	XMLName          xml.Name          `xml:"Schema"`
+	EntityTypes      []EntityType      `xml:"EntityType"`
+	EntityContainers []EntityContainer `xml:"EntityContainer"`
+	XMLNS            string            `xml:"xmlns,attr"`
+	Namespace        string            `xml:"Namespace"`
+}
+
+type EntityContainer struct {
+	XMLName    xml.Name    `xml:"EntityContainer"`
+	Name       string      `xml:"Name,attr"`
+	EntitySets []EntitySet `xml:"EntitySet"`
+}
+
+type EntitySet struct {
+	XMLName    xml.Name `xml:"EntitySet"`
+	Name       string   `xml:"Name,attr"`
+	EntityType string   `xml:"EntityType,attr"`
+}
+
+type EntityType struct {
+	XMLName    xml.Name   `xml:"EntityType"`
+	Key        EntityKey  `xml:"Key"`
+	Name       string     `xml:"Name,attr"`
+	Properties []Property `xml:"Property"`
+}
+
+type EntityKey struct {
+	XMLName      xml.Name      `xml:"Key"`
+	PropertyRefs []PropertyRef `xml:"PropertyRef"`
+}
+
+type PropertyRef struct {
+	XMLName xml.Name `xml:"PropertyRef"`
+	Name    string   `xml:"Name,attr"`
+}
+
+type Property struct {
+	XMLName  xml.Name `xml:"Property"`
+	Name     string   `xml:"Name,attr"`
+	Type     string   `xml:"Type,attr"`
+	Nullable string   `xml:"Nullable,attr"`
+}
